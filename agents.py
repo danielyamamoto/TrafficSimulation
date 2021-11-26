1fea8f7578345b3ded5d3415de718c229d8ac268
danielyamamoto <danylarayamamoto@hotmail.com>
2021-11-25 21:47:07 -0600

    Vehicles updated

--- agents.py
+++ agents.py
@@ -27,10 +27,15 @@ class TrafficLight(Agent):
         return random.choice(colors)
 
 class Vehicle(Agent):
-    def __init__(self, unique_id, pos, model):
+    def __init__(self, unique_id, pos, ori, model):
         super().__init__(unique_id, model)
-        self.pos_x = 1
-        self.pox_y = 1
+        self.pos_x = pos[0]
+        self.pox_y = pos[1]
+        self.ori_x = ori[0]
+        self.ori_y = ori[1]
+
+    def boundaries(self, pos):
+        return pos[0] > 0 and pos[0] < self.model.size and pos[1] > 0 and pos[1] < self.model.size
 
     def step(self):
         print("I'm a vehicle " + str(self.unique_id))
--- model.py
+++ model.py
@@ -6,11 +6,12 @@ from agents import TrafficLight, Vehicle
 from schedule import RandomActivationByType
 
 class StreetModel(Model):
-    def __init__(self, nVehicles = 4, nLights = 4, size = 20):
+    def __init__(self, nVehicles = 4, nLights = 4, nSize = 20):
         super().__init__()
         self.num_vehicles = nVehicles
         self.num_lights = nLights
-        self.grid = MultiGrid(size, size, False)
+        self.size = nSize
+        self.grid = MultiGrid(nSize, nSize, False)
         self.schedule = RandomActivationByType(self)
 
         self.datacollector = DataCollector(
@@ -20,12 +21,17 @@ class StreetModel(Model):
             }
         )
         
+        origin_pos = [[10,0],[0,9],[9,19],[19,10]]
+        orient = [[0,1],[1,0],[0,-1],[-1,0]]
         # Create Vehicles
         for i in range(self.num_vehicles):
-            x = self.random.randrange(20)
-            y = self.random.randrange(20)
-            v = Vehicle(i, (x, y), self)
-            self.grid.place_agent(v, (x, y))
+            rand = self.random.randint(0, 3)
+            x1 = origin_pos[rand][0]
+            y1 = origin_pos[rand][1]
+            x2 = orient[rand][0]
+            y2 = orient[rand][1]
+            v = Vehicle(i, (x1, y1), (x2, y2), self)
+            self.grid.place_agent(v, (x1, y1))
             self.schedule.add(v)
 
         pos_lig = [[8,9],[9,11],[10,8],[11,10]]
